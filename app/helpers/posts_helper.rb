b9a30b7e9dc1421cc5e4f8286c42268ec3051781
Davide D'Agostino <d.dagostino@lipsiasoft.com>
2010-03-19 16:04:13 +0100

    Changed auto_discovery_link_tag => feed_tag

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,9 +1,9 @@
 # Helper methods defined here can be accessed in any controller or view in the application
 
 SampleBlog.helpers do
-  # auto_discovery_link_tag(:rss, :title => "RSS", :href => url(...))
-  # auto_discovery_link_tag(:atom, :title => "ATOM", :href => url(...))
-  def auto_discovery_link_tag(mime, options={})
+  # feed_tag(:rss, :title => "RSS", :href => url(...))
+  # feed_tag(:atom, :title => "ATOM", :href => url(...))
+  def feed_tag(mime, options={})
     full_mime = (mime == :atom) ? 'application/atom+xml' : 'application/rss+xml'
     content_tag(:link, options.reverse_merge(:rel => 'alternate', :type => full_mime))
   end
